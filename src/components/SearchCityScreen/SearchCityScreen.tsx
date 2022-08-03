69b6b6cf32f80155d068fe6aa16f6204db3a4932
Pablo Henrique Castelo <pablohenriquec7@gmail.com>
2022-08-03 19:39:31 -0300

    adicionados os componentes do footer

--- src/components/SearchCityScreen/SearchCityScreen.tsx
+++ src/components/SearchCityScreen/SearchCityScreen.tsx
@@ -11,10 +11,30 @@ export const SearchCityScreen: React.FC<SearchCityScreenProps> = () => {
         <header className="header">
           <ToggleWeatherButton />
         </header>
-        <main>
+        <main className="container">
           <h1>Como está o tempo hoje?</h1>
           <AutocompleteInput />
         </main>
+        <footer>
+          <div className="select-lang-button">
+            <img
+              src="src\assets\brazil.png"
+              alt="bandeira do Brasil"
+              height={20}
+            />
+            <img
+              src="src\assets\united-states.png"
+              alt="bandeira dos Estados Unidos"
+              height={20}
+            />
+            <img
+              src="src\assets\spain.png"
+              alt="bandeira da Espanha"
+              height={20}
+            />
+          </div>
+          <div>Idioma selecionado: Português</div>
+        </footer>
       </div>
     </>
   )
